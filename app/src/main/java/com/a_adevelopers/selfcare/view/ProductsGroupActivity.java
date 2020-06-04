afb0c5cc0f112a565025997ed0cd80323708c58c
andrewhpaul <55168970+andrewhpaul@users.noreply.github.com>
2020-06-04 12:01:26 +1000

    Added return buttons

--- app/src/main/java/com/a_adevelopers/selfcare/view/ProductsGroupActivity.java
+++ app/src/main/java/com/a_adevelopers/selfcare/view/ProductsGroupActivity.java
@@ -14,6 +14,7 @@ import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -38,10 +39,20 @@ public class ProductsGroupActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     GroupProductAdapter adapter;
     SqlLite sql;
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_products_group );
+
+        back=findViewById( R.id.back );
+        back.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        } );
+
         database=new SqlLite( this );
         if(getIntent().getStringExtra( "ID" ).equals( "g_id" )){
             Toast.makeText( this, "popofdsf", Toast.LENGTH_SHORT ).show();
--- app/src/main/java/com/a_adevelopers/selfcare/view/SelecteNotificationProduct.java
+++ app/src/main/java/com/a_adevelopers/selfcare/view/SelecteNotificationProduct.java
@@ -2,8 +2,11 @@ package com.a_adevelopers.selfcare.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.a_adevelopers.selfcare.Adapter.Adapter;
 import com.a_adevelopers.selfcare.Adapter.SelectAdapterNotification;
@@ -11,14 +14,25 @@ import com.a_adevelopers.selfcare.MainActivity;
 import com.a_adevelopers.selfcare.R;
 import com.a_adevelopers.selfcare.db.SqlLite;
 
+
 public class SelecteNotificationProduct extends AppCompatActivity {
     RecyclerView mRecyclerView;
     SelectAdapterNotification adapter;
     private SqlLite databaseHelper;
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_selecte_notification_product );
+
+        back=findViewById( R.id.back );
+        back.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        } );
+
         mRecyclerView = findViewById(R.id.recycler);
         databaseHelper = new SqlLite(this);
         showRecord();
